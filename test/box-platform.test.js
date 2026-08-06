0f8bd36acc5d787ea00e6ce2be7a773c8441e8a0
doge-liang <33837949+doge-liang@users.noreply.github.com>
2026-08-06 00:55:30 +0000

    fix: isolate platform path tests from runner env

--- test/box-platform.test.js
+++ test/box-platform.test.js
@@ -4,9 +4,9 @@ const assert = require('node:assert');
 const { platformPaths } = require('../lib/platform');
 
 test('platform paths use XDG, macOS and Windows app-data roots', () => {
-  assert.equal(platformPaths('linux', '/home/a').configDir, '/home/a/.config/ag-box');
-  assert.equal(platformPaths('darwin', '/Users/a').configDir, '/Users/a/Library/Application Support/ag-box');
-  assert.equal(platformPaths('win32', 'C:\\Users\\a').configDir, 'C:\\Users\\a\\AppData\\Roaming\\ag-box');
+  assert.equal(platformPaths('linux', '/home/a', {}).configDir, '/home/a/.config/ag-box');
+  assert.equal(platformPaths('darwin', '/Users/a', {}).configDir, '/Users/a/Library/Application Support/ag-box');
+  assert.equal(platformPaths('win32', 'C:\\Users\\a', {}).configDir, 'C:\\Users\\a\\AppData\\Roaming\\ag-box');
 });
 
 test('platform paths honor injected XDG and APPDATA overrides', () => {
